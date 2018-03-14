497a6ae7245499e53d81fec89a754b2b6ff260df
kateleecee <cynthialee.lee525@gmail.com>
2018-03-14 15:45:01 -0700

    set up webpack, babel and react

---
+++ Calculator/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const config = {
+    entry: path.join(__dirname, '/client/index.js'),
+    output:{
+        path: path.join(__dirname, './public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader'
+        }]
+    }
+};
+
+module.exports = config;
